547aa2ea4c356be510b254c0635e494f4d24390c
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-01-30 16:09:58 +0800

    feat: vue2 子應用新增微服務調用方法

--- page-energy-vue2/src/main.js
+++ page-energy-vue2/src/main.js
@@ -4,7 +4,30 @@ import router from './router'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+let instance = null
+
+const render = () => {
+  instance = new Vue({
+    router,
+    render: h => h(App)
+  }).$mount('#app')
+}
+
+// NOTE: 當前若不是在微前端環境, 才直接執行 render()
+if (!window.__MICRO_WEB__) {
+  render()
+}
+
+// NOTE: 提供給微前端主框架調用的方法
+export const bootstrap = () => {
+  console.log('開始加載')
+}
+
+export const mount = () => {
+  render()
+  console.log('渲染成功')
+}
+
+export const unmount = () => {
+  console.log('卸載', instance)
+}
